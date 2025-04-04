b29fc62240b11cfa844d2cea83b7caeea2adf0ca
Tanvi Jha <154341594+Tanviriya@users.noreply.github.com>
2025-04-04 14:49:46 +0530

    Add files via upload

--- facereco.py
+++ facereco.py
@@ -23,24 +23,27 @@ while True:    #now we have to read the run time images for that
     ret, video_data=video_cap.read()
     #we made two varaiable aone we are using for reading the live image (.read()) method
     #for creatring the frame of video
+    #ret tells whethr the frame was succesfully captured or not
     col=cv2.cvtColor(video_data,cv2.COLOR_BGR2GRAY)
 
 # BAscially we changed our video color using cv2.cvtColor method into black and white color using cv2.COLOR_BGR2GRAY
 #so that face recognition could capture the details of the face
-    faces=face_cap.detectMultiScale(
-        col,
-        scaleFactor=1.1, 
-        minNeighbors=5,
-        minSize=(30,30),
-        flags=cv2.CASCADE_SCALE_IMAGE
+    faces=face_cap.detectMultiScale(  #detecting the faces in the frame
+        col, #gray color 
+        scaleFactor=1.1,  #reduces images size by 10 % at each scale 
+        minNeighbors=5, #the number of rectangel that should detect around a face 
+        minSize=(40,40),  #thr minimum size of detected face
+        flags= cv2.CASCADE_SCALE_IMAGE
         )
-    for(x,y,w,h) in faces:
-        cv2.rectangle(video_data,(x,y),(x+w,y+h),(255,255,0),2)
-
+    for(x,y,w,h) in faces:       #(x,y),(x+w,y+h) the cordinates of the rectangle
+        cv2.rectangle(video_data,(x,y),(x+w,y+h),(255,255,0),2) #2 is the thickness
+    #if face is detected opencv returns the coordinates 
+    #x,y -top left corner of the face
+    #wh width and height
 
     cv2.imshow("camera",video_data)
     #using imshow  we are creating a frame and then we gave it a name and the the data
-    if cv2.waitKey(10)== ord("a"):
+    if cv2.waitKey(10)== ord("t"):
         break
     #we used waitkey method to let run the image till given duration in milllisecond
     #ord used for the key press function giving ascii unicode 
